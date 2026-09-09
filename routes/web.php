7f41995e9dc0ae7eb46726775bdb91d97421616a
Rhode Visser <rosarhode@gmail.com>
2026-09-09 10:32:05 +0200

    fix: Update login name
    Guest visiting /profile/{user} got laravel error, because it had no name. Now guest will be redirected to the login page.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::prefix('register')->name('register.')->group(function () {
