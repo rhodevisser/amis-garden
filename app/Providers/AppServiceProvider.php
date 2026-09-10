bd7f99bcee48d4e8875f0a749ab458f4c08e1812
kapufskipauw <rosarhode@gmail.com>
2026-09-10 11:06:16 +0200

    fix: Added Eloquent strictness
    Now it has N+1 prevention and a warning when trying to set an unfillable property

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(! $this->app->isProduction());
+        Model::preventSilentlyDiscardingAttributes(! $this->app->isProduction());
+
     }
 }
